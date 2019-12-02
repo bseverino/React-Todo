e82fa4f756795902c312b44e75fb58328af1dc51
B Severino <biancasev@gmail.com>
2019-12-02 15:58:41 -0500

    Completed add task button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const todo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todo: todo,
+      inputField: ''
+    };
+  };
+
+  addTask = newTaskText => {
+    const newTask = {
+      task: newTaskText,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      todo: [...this.state.todo, newTask]
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      inputField: e.target.value   
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.addTask(this.state.inputField);
+    this.setState({
+      inputField: ''
+    });
+  };
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To-Do List</h2>
+        <TodoList todo={this.state.todo} />
+        <TodoForm inputField={this.state.inputField} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit}>
+                    <input type='text' name='task' value={this.props.inputField} onChange={this.props.handleChange} />
+                    <button>Add Task</button>
+                </form>
+                <button>Clear Completed</button>
+            </div>
+        );
+    };
+};
+
+export default TodoForm;
