fcf3a09b634d9ed2e7662fa7da262704e1154687
B Severino <biancasev@gmail.com>
2019-12-02 18:03:00 -0500

    Clean-up, changed title

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import './components/TodoComponents/Todo.css';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
-const todo = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-];
-
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
